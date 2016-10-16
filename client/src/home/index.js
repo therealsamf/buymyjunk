d30b2ac698ab6cbc057ca37a299d1974d5b060f3
therealsamf <srf.000@hotmail.com>
2016-10-15 20:25:32 -0500

    Updated .gitignore, refactored home/index.js and moved the modal into it's own file

--- client/src/home/index.js
+++ client/src/home/index.js
@@ -1,12 +1,14 @@
 
 const React = require('react');
+const ReactDom = require('react-dom');
 const ReactBootstrap = require('react-bootstrap');
 
 const {Jumbotron, FormGroup, ControlLabel, HelpBlock, FormControl, 
   Modal, Form, Col, Checkbox, Button, ListGroupItem, ListGroup} = ReactBootstrap;
 
-const ReactDom = require('react-dom');
 const Styles = require('./style.js');
+
+const LoginModal = require('./LoginModal.js');
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -17,7 +19,6 @@ class HomePage extends React.Component {
   }
 
   cancel() {
-    console.log('Closing!');
     this.setState({ showModal: false });
   }
 
@@ -26,6 +27,7 @@ class HomePage extends React.Component {
   }
 
   render() {
+    var _this = this;
     return(
       <Jumbotron style={{'backgroundColor': '#27ae60', 'textAlign': 'center', 'minWidth': '100%', 'minHeight': screen.height.toString() + 'px'}}>
         <h1 style={Styles.container}>{'Buy My Junk!'}</h1>
@@ -60,57 +62,17 @@ class HomePage extends React.Component {
                   Sign in
                 </Button>
                 {' '}
-                <Button bsStyle={"primary"}  onClick={this.open.bind(this)}>
+                <Button bsStyle={"primary"}  onClick={function() {
+                  if (_this.refs.LoginModal) {
+                    _this.refs.LoginModal.open();
+                  }
+                }}>
                     Sign Up
                 </Button>         
               </Col>
             </FormGroup>
           </Form>
-          <Modal show={this.state.showModal}>
-            <Modal.Header>
-              <Modal.Title></Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                  <Form horizontal style={{'textAlign': 'center', 'marginRight': '15%'}}>
-                     <FormGroup controlId="formHorizontalEmail">
-                        <Col componentClass={ControlLabel} sm={2}>
-                           Email
-                        </Col>
-                        <Col sm={10}>
-                          <FormControl type="email" placeholder="Email" />
-                        </Col>
-                      </FormGroup>
-                       <FormGroup controlId="formHorizontalEmail">
-                        <Col componentClass={ControlLabel} sm={2}>
-                           Verify
-                        </Col>
-                        <Col sm={10}>
-                          <FormControl type="email" placeholder="Verifcation Code" />
-                        </Col>
-                      </FormGroup>
-                       <FormGroup controlId="formHorizontalEmail">
-                        <Col componentClass={ControlLabel} sm={2}>
-                           Username
-                        </Col>
-                        <Col sm={10}>
-                          <FormControl type="email" placeholder="Username" />
-                        </Col>
-                      </FormGroup>
-                      <FormGroup controlId="formHorizontalPassword">
-                      <Col componentClass={ControlLabel} sm={2}>
-                        Password
-                      </Col>
-                      <Col sm={10}>
-                        <FormControl type="password" placeholder="Password" />
-                      </Col>
-                    </FormGroup>
-                 </Form>
-                </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={this.cancel.bind(this)}>Cancel</Button>
-              <Button bsStyle="primary">Submit</Button>
-            </Modal.Footer>
-          </Modal>
+          <LoginModal ref={'LoginModal'} />
           <h2>Items Sold</h2>
           <span>
             <ul >
